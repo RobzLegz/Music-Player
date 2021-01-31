e7809bb4c5d1da09a26f9eb26e70a34b0e174d21
Robzlegz <robzlegz@gmail.com>
2021-01-31 17:25:35 +0200

    adding all imports

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import './App.css';
 import Nav from "./Nav";
+import Song from "./Song";
+import Player from "./Player";
+import Library from "./Library";
 
 const App = () => {
   return (
     <div className="app">
       <Nav />
-      
+      <Song />
     </div>
   );
 }
