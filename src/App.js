1a0a50b9d270c98f5304b602aa2bbe995ae6a0a8
Robzlegz <robzlegz@gmail.com>
2021-01-31 17:33:27 +0200

    importing song data

--- src/App.js
+++ src/App.js
@@ -4,14 +4,24 @@ import Nav from "./Nav";
 import Song from "./Song";
 import Player from "./Player";
 import Library from "./Library";
+import songData from "./songData";
 
 const App = () => {
+
+  const [libraryStatus, setLibraryStatus] = useState(false);
+  const audioRef = useRef(null);
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [songInfo, setSongInfo] = useState({currentTime: 0, duration: 0,animationPercentage: 0,});
+
   return (
     <div className="app">
       <Nav />
       <Song />
       <Player />
       <Library />
+      <audio></audio>
     </div>
   );
 }
